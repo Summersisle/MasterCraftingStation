102ec025fa007c10c9b06dd2bc237d0bba8c3ab5
Summersisle <25886892+Summersisle@users.noreply.github.com>
2022-03-10 22:05:19 -0800

    Example Sword

---
+++ Dusts/ExampleSolution.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace MasterCraftingStation.Dusts
+{
+	public class ExampleSolution : ModDust
+	{
+		public override void SetStaticDefaults() {
+			UpdateType = 110;
+		}
+	}
+}
